b868e731e28bef1a58bff3e69a97c609eff86550
Piotr Sobieszczyk <51410953+Sobiechh@users.noreply.github.com>
2021-02-25 04:01:32 +0100

    utils import fix

--- otodom.py
+++ otodom.py
@@ -2,7 +2,7 @@ from bs4 import BeautifulSoup
 import requests
 from selenium import webdriver
 import time
-from utils import get_url_content
+from utils import get_url_content, txt_to_float
 
 """
 Get content from otodom page
