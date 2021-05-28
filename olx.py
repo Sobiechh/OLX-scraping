d8f07f75bf2abde97946e2f479f43be594896766
Piotr Sobieszczyk <51410953+Sobiechh@users.noreply.github.com>
2021-05-28 02:03:03 +0200

    olx + otodom get info changes

--- olx.py
+++ olx.py
@@ -1,52 +1,49 @@
-from bs4 import BeautifulSoup
-import requests
+from utils import txt_to_float
 
 """
 Get content from olx page
 """
-TEST_URL = "https://www.olx.pl/oferta/dzialka-pod-farme-fotowoltaiczna-CID3-IDFMtbN.html#b45bf9918b"
-TEST_URL2 ="https://www.olx.pl/oferta/sprzedam-dzialke-w-rod-ksiezy-mlyn-CID3-IDIlPPY.html#3971846459;promoted"
 
 def get_bottom_bar(soup):
-    bottom_bar = soup.find_all(class_="offer-bottombar__item")
+    bottom_bar = soup.find(class_="css-1v2fuie")
 
     return bottom_bar
 
 def get_offer_id(bottom_bar):
-    offer_id = bottom_bar[-1].find("strong").get_text()
-
-    return offer_id
+    offer_id = bottom_bar.find(class_="css-7oa68k-Text").get_text()
+    
+    return int(offer_id.split(' ')[-1])
 
 def get_details(soup):
-    offer_details = soup.find_all(class_="offer-details__item")
+    offer_details = soup.find(class_="css-sfcl1s")
 
     return offer_details
     
 def get_offer_surface(details):
-    surface_txt = details[2].find("strong").get_text()
-
+    details_items = details.find_all(class_="css-ox1ptj")
+    surface_txt = details_items[-2].get_text().split(' ', 1)[-1]
     surface = txt_to_float(surface_txt)
 
     return surface
 
 def get_offer_title(soup):
-    title = soup.find(class_='offer-titlebox').find('h1').get_text().strip()
+    title = soup.find('h1', {"data-cy": "ad_title"}).get_text().strip()
 
     return title
 
 def get_offer_localization(soup):
-    localization_name = soup.find(class_='offer-user__address').p.get_text().split(',')[0].strip()
-    
+    localization_name = soup.find('p', {"class": "css-7xdcwc-Text"})\
+        .get_text().split(',')[0].strip()
+
     return localization_name
 
 def get_offer_description(soup):
-    description =  soup.find(id='textContent').get_text().strip()
+    description =  soup.find(class_='css-g5mtbi-Text').get_text().strip()
     
     return description
 
 def get_offer_price(soup):
-    price_txt = soup.find(class_="pricelabel__value arranged").get_text()
-
+    price_txt = soup.find(class_="css-8kqr5l-Text").get_text()
     price = txt_to_float(price_txt)
 
     return price
@@ -59,13 +56,9 @@ def txt_to_float(text):
 
     return parsed
 
-def get_offer_infos(url):
-    page = requests.get(url)
-    soup = BeautifulSoup(page.content, "html.parser")
-
+def get_offer_infos(soup, url_link):
     bottom_bar = get_bottom_bar(soup)
     details = get_details(soup)
-
     """
     offer_id -> get from bottom bar
     offer_surfacet -> get from details at the top of page
@@ -79,6 +72,7 @@ def get_offer_infos(url):
     offer_price = get_offer_price(soup)
 
     details = {
+        "url_link": url_link,
         "id": offer_id,
         "surface": offer_surface,
         "title": offer_title,
@@ -88,6 +82,3 @@ def get_offer_infos(url):
     }
 
     return details
-
-print(get_offer_infos(TEST_URL))
-print(get_offer_infos(TEST_URL2))
--- otodom.py
+++ otodom.py
@@ -1,40 +1,33 @@
-from bs4 import BeautifulSoup
-import requests
-from selenium import webdriver
-import time
-from utils import get_url_content, txt_to_float
+from utils import txt_to_float
 
 """
 Get content from otodom page
 """
-TEST_URL = "https://www.otodom.pl/pl/oferta/malanow-promocja-dzialki-budowlane-ID3Z50c.html?"
-TEST_URL2 ="https://www.otodom.pl/pl/oferta/dzialka-projekt-pozwolenie-na-budowe-fak-vat-ID49gOh.html#08fbc89bdc"
-
 
 def get_offer_id(soup):
     
-    offer_id_desc = soup.find("div", {"class" : "css-jjerc6 edhdn7k3"})
-    offer_id = int(offer_id_desc.text.split(" ")[-1])
+    offer_id_desc = soup.find("div", {"class" : "css-jjerc6"})
+    offer_id = int(offer_id_desc.text.split(" ")[-1]) if offer_id_desc else None
 
     return offer_id
     
 
 def get_offer_surface(soup):
 
-    surface_txt = soup.find("div", {"class" : "css-1ytkscc ecjfvbm0"})
-    surface = txt_to_float(surface_txt.text)
+    surface_txt = soup.find("div", {"class" : "css-1ytkscc"})
+    surface = txt_to_float(surface_txt.text) if surface_txt else None
 
     return surface
 
 def get_offer_title(soup):
 
-    title = soup.find("h1", {"class" : "css-46s0sq edo911a18"})
+    title = soup.find("h1", {"class" : "css-46s0sq"})
 
     return title.text
 
 def get_offer_localization(soup):
 
-    localization_name = soup.find("a", {"class" : "css-1qz7z11 eom7om61"})
+    localization_name = soup.find("a", {"class" : "css-1qz7z11"})
     localization_name = localization_name.text.split(",")[0].strip()
 
     return localization_name
@@ -53,9 +46,7 @@ def get_offer_price(soup):
 
     return price
 
-def get_offer_infos(url):
-    soup = get_url_content(url, None)
-
+def get_offer_infos(soup, url_link):
     offer_id = get_offer_id(soup)
     offer_surface = get_offer_surface(soup)
     offer_title = get_offer_title(soup)
@@ -64,6 +55,7 @@ def get_offer_infos(url):
     offer_price = get_offer_price(soup)
 
     details = {
+        "url_link": url_link,
         "id": offer_id,
         "surface": offer_surface,
         "title": offer_title,
@@ -73,9 +65,3 @@ def get_offer_infos(url):
     }
 
     return details
-
-details = get_offer_infos(TEST_URL)
-details2 = get_offer_infos(TEST_URL2)
-
-print(details)
-print(details2)
